2fdf3d60e6eed9a32b0fb87632149699c12d02c7
et code <edgar.topete@gmail.com>
2019-06-29 00:24:44 -0500

    workin index

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,18 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-
+ var service=null;
   app.get("/", function(req, res) {
-    res.render("index");
+    db.Services.findAll({limit:3}).then(function(dbService) {
+      service=dbService;
+    });
+    //res.render("index");
+        db.Store.findAll({limit:3}).then(function(dbStores) {
+        res.render("index", {
+           stores: dbStores,
+           service: service
+         });
+      });
   });
   // app.get("/", function(req, res) {
   //   db.Example.findAll({}).then(function(dbExamples) {
